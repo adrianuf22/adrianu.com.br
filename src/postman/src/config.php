eb23faabce5025107cd5f4472998961b25ebf41b
Adriano <adrianu_f22@hotmail.com>
2016-10-08 22:39:02 +0000

    Old version deleted

--- src/postman/src/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// panel/config.php
-
-require_once __DIR__ . '/../vendor/autoload.php';
--- src/postman/src/system/exception.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-
-	class Exception {
-		public function Ok () {
-			return 'ok';
-		}
-	}
