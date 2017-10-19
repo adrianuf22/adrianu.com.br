011cd2147a9b01b130365c0945f782f72eb364e1
adrianuf22 <adrianu_f22@hotmail.com>
2017-10-19 01:31:40 -0200

    Implementa script de deploy - parcial

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-#not implemented
+
+toIgnore = (
+    ".travis.yml"
+    "deploy.sh"
+    "package.json"
+    "gruntfile.js"
+    "composer.*"
+)
+
+lastCommit=$(git log --format="%H" -n 1)
+echo $lastCommit
+
+filesChanged=$(git diff-tree --no-commt-id --name-only -r $lastCommit)
+
+if [ ${#filesChanged[@]} -eq 0 ]; then
+    echo "No files to update, aborting..."
+    exit 0;
+fi
+
+for file in $filesChanged
+do
+
+done
