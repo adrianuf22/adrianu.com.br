ce2a5e4fe757f12124349c1f6a64801553b92594
adrianuf22 <adrianu_f22@hotmail.com>
2017-10-31 01:03:26 -0200

    Variaveis de ambiente

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
 
-toIgnore = (
-    ".travis.yml"
-    "deploy.sh"
-    "package.json"
-    "gruntfile.js"
-    "composer.*"
-)
-
-lastCommit=$(git log --format="%H" -n 1)
-echo $lastCommit
-
-filesChanged=$(git diff-tree --no-commt-id --name-only -r $lastCommit)
-
-if [ ${#filesChanged[@]} -eq 0 ]; then
-    echo "No files to update, aborting..."
-    exit 0;
-fi
-
-for file in $filesChanged
-do
-
-done
+find ./www -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://$FTP_HOST$FTP_HOME/{} \;
